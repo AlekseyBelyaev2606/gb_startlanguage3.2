61b9fc3be661ef637a2abfa144b36e281ef3249d
AlekseyBelyaev2606 <partchoice44@gmail.com>
2023-02-02 21:36:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите координаты точки A, ось X:");
+int a1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Далее ось Y");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Далее ось Z");
+int c1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B, ось X: ");
+int a2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Далее ось Y");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Далее ось Z");
+int c2 = int.Parse(Console.ReadLine());
+
+double x = Math.Sqrt(Math.Pow(a1 - a2, 2) + Math.Pow(b1 - b2,2) + Math.Pow(c1 - c2,2));
+Console.WriteLine(x);
